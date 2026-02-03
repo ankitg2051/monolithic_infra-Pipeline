be86a1e4cc0eec5b37711232274d484ed1f2547b
Your Name <you@example.com>
2026-02-04 02:23:00 +0530

    create RG code with generic module

---
+++ Environment/dev/variable.tf
@@ -0,0 +1,25 @@
+variable "rgs" {
+  type = map(object({
+    name       = string
+    location   = string
+    managed_by = optional(string)
+    tags       = optional(map(string))
+    timeouts = optional(object({
+      create = optional(string)
+      delete = optional(string)
+      read   = optional(string)
+      update = optional(string)
+    }))
+  }))
+}
+
+
+variable "str" {
+  type = map(object({
+    name                     = string
+    location                 = string
+    resource_group_name      = string
+    account_tier             = string
+    account_replication_type = string
+  }))
+}
---
+++ Modules/Storage_account/variable.tf
@@ -0,0 +1,9 @@
+variable "str" {
+  type = map(object({
+    name                     = string
+    location                 = string
+    resource_group_name      = string
+    account_tier             = string
+    account_replication_type = string
+  }))
+}
